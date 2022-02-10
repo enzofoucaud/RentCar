72e07226153eb50b9431f4b0f73f47b8fa478ea9
Enzo Foucaud <foucauden@gmail.com>
2022-02-10 10:25:37 +0100

    New: First view of classes content

--- RentCar/Car.cs
+++ RentCar/Car.cs
@@ -4,5 +4,13 @@ namespace RentCar
 {
     public class Car
     {
+        public string matriculation;
+        public string brand;
+        public string model;
+        public string color;
+        public double form_price;
+        public double cleaning_price;
+        public double price_km;
+        public int horsepower;
     }
 }
--- RentCar/Driver.cs
+++ RentCar/Driver.cs
@@ -6,5 +6,16 @@ namespace RentCar
 {
     class Driver
     {
+        public string lastname;
+        public string firstname;
+        public string birthday;
+        public string driver_licence_date;
+        public string driver_licence_id;
+
+        public Driver(string _lastname, string _firstname)
+        {
+            this.lastname = _lastname;
+            this.firstname = _firstname;
+        }
     }
 }
